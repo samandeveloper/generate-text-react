eccd2c83940a40ea79ea642b6bd5cebd178e7824
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-08 22:12:54 -0700

    Updates

---
+++ precache-manifest.3a8c70f294a8ec02857c493731d6102c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311b6b78e367a75365172eb52731b5f2",
+    "url": "/generate-text-react/index.html"
+  },
+  {
+    "revision": "c340ba7cbb063cf1a466",
+    "url": "/generate-text-react/static/css/main.05b7e018.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/generate-text-react/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/generate-text-react/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c340ba7cbb063cf1a466",
+    "url": "/generate-text-react/static/js/main.14d3b37a.chunk.js"
+  },
+  {
+    "revision": "bce29d9996397be593c6",
+    "url": "/generate-text-react/static/js/runtime-main.c7b8bf1b.js"
+  }
+]);
